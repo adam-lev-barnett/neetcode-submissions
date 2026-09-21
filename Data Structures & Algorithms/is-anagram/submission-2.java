30623390c49f529a1bd6fdebe05ccec733261aab
Adam Barnett <adam.lev.barnett@gmail.com>
2026-09-21 13:53:44 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] counts1 = new int[26];
+        int[] counts2 = new int[26];
+
+        if (s.length() != t.length()) return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            counts1[Character.toLowerCase(s.charAt(i)) - 'a']++;
+            counts2[Character.toLowerCase(t.charAt(i)) - 'a']++;
+        }
+
+        return Arrays.equals(counts1, counts2);
+    }
+}
